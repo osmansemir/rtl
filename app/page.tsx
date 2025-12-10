0fca3e5cdbd4e061939399c67430ceaea8f0f24a
Osman Semir <osmanshek@gmail.com>
2025-12-10 22:30:37 +0400

    Add additional css

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import Hero from "@/components/hero-section";
+import PopularDestinations from "@/components/popular-destinations";
+import StartPlanning from "@/components/start-planning";
+import Testimonials from "@/components/testimonials";
+import WhyChooseUs from "@/components/why-choose-us";
+
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <div className="travel min-h-screen bg-white">
+        <Header />
+        <Hero />
+        <PopularDestinations />
+        <WhyChooseUs />
+        <Testimonials />
+        <StartPlanning />
+        <Footer />
+      </div>
+    </div>
+  );
 }
